4d9d108ad33e691e0bea796ee38e6943140716f1
Ardis Lu <ardisglu@gmail.com>
2023-09-10 20:57:00 -0700

    Update binary

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [[ "$1" == '--dev' ]]; then
   source ./.env.dev
 fi
 
-curl https://redbean.dev/redbean-2.0.7.com >redbean.com
+curl https://redbean.dev/redbean-2.2.com >redbean.com
 zip redbean.com $FILES
